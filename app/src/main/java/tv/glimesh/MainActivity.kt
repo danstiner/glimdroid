0619a3829c2c9b14173d24a0c98c24fe7f14e55f
Daniel Stiner <danstiner@gmail.com>
2022-01-11 00:13:00 -0800

    Padding and actionbar touch-ups

--- app/src/main/java/tv/glimesh/MainActivity.kt
+++ app/src/main/java/tv/glimesh/MainActivity.kt
@@ -1,7 +1,13 @@
 package tv.glimesh
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -15,8 +21,11 @@ import tv.glimesh.ui.main.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
+
     private lateinit var mainViewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val drawerLayout = binding.drawerLayout
         val navView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -48,8 +58,57 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(drawerToggle)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+
         // TODO link to liveview
-        showFollowingCount(42)
+        showFollowingCount(2)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_activity_actions, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                Log.d(TAG, "onOptionsItemSelected: home")
+//                if (drawerLayout.isDrawerOpen(drawerList)) {
+//                    drawerLayout.closeDrawer(drawerList)
+//                } else {
+//                    drawerLayout.openDrawer(drawerList)
+//                }
+                true
+            }
+            R.id.action_user_profile -> {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://glimesh.tv/users/settings/profile")
+                    )
+                )
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun showFollowingCount(count: Int) {
