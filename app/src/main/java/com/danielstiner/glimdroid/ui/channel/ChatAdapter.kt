23340d560890fe6c5355e0312c573cced0d373a7
Daniel Stiner <danstiner@gmail.com>
2022-01-19 09:58:51 -0800

    Cleanup unused code

--- app/src/main/java/com/danielstiner/glimdroid/ui/channel/ChatAdapter.kt
+++ app/src/main/java/com/danielstiner/glimdroid/ui/channel/ChatAdapter.kt
@@ -1,14 +1,9 @@
 package com.danielstiner.glimdroid.ui.channel
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.text.Html
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ImageSpan
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,15 +14,10 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.Priority
-import com.bumptech.glide.request.Request
 import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.target.SizeReadyCallback
-import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.danielstiner.glimdroid.R
 import com.danielstiner.glimdroid.data.ChatMessage
-import java.lang.ref.WeakReference
 import java.net.URL
 
 class ChatAdapter :
@@ -38,7 +28,6 @@ class ChatAdapter :
         private val textView: TextView = itemView.findViewById(R.id.text)
 
         fun bind(chat: ChatMessage) {
-            Log.d("ChatAdapter", chat.message)
             textView.text = buildSpannedString {
                 append("  ")
                 bold { append(chat.displayname) }
@@ -113,112 +102,4 @@ inline fun SpannableStringBuilder.appendSpan(
     append(text)
     setSpan(what, start, length, flags)
     return this
-}
-
-
-class GlideImageGetter(
-    textView: TextView,
-    private val width: Int,
-    private val height: Int,
-    densityAware: Boolean = false,
-    private val imagesHandler: HtmlImagesHandler? = null
-) : Html.ImageGetter {
-    private val container: WeakReference<TextView> = WeakReference(textView)
-    private var density = 1.0f
-
-    init {
-        if (densityAware) {
-            container.get()?.let {
-                density = it.resources.displayMetrics.density
-            }
-        }
-    }
-
-    override fun getDrawable(source: String): Drawable {
-        imagesHandler?.addImage(source)
-
-        val drawable = BitmapDrawablePlaceholder()
-
-        // Load Image to the Drawable
-        container.get()?.apply {
-            post {
-                Glide.with(context)
-                    .asBitmap()
-                    .load(source)
-                    .priority(Priority.LOW)
-                    .circleCrop()
-                    .into(drawable)
-            }
-        }
-
-        return drawable
-    }
-
-    private inner class BitmapDrawablePlaceholder : BitmapDrawable(
-        container.get()?.resources,
-        Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
-    ), Target<Bitmap> {
-        private var drawable: Drawable? = null
-            set(value) {
-                field = value
-                value?.let { drawable ->
-                    val drawableWidth = (width * density).toInt()
-                    val drawableHeight = (height * density).toInt()
-                    val maxHeight = container.get()!!.measuredHeight
-                    if (drawableHeight > maxHeight) {
-                        val calculatedWidth = maxHeight * drawableHeight / drawableWidth
-                        drawable.setBounds(0, 0, calculatedWidth, maxHeight)
-                        setBounds(0, 0, calculatedWidth, maxHeight)
-                    } else {
-                        drawable.setBounds(0, 0, drawableWidth, drawableHeight)
-                        setBounds(0, 0, drawableWidth, drawableHeight)
-                    }
-                    container.get()?.text = container.get()?.text
-                }
-            }
-
-        override fun draw(canvas: Canvas) {
-            drawable?.draw(canvas)
-        }
-
-        override fun onLoadStarted(placeholderDrawable: Drawable?) {
-            placeholderDrawable?.let {
-                drawable = it
-            }
-        }
-
-        override fun onLoadFailed(errorDrawable: Drawable?) {
-            errorDrawable?.let {
-                drawable = it
-            }
-        }
-
-        override fun onResourceReady(bitmap: Bitmap, transition: Transition<in Bitmap>?) {
-            drawable = BitmapDrawable(container.get()!!.resources, bitmap)
-        }
-
-        override fun onLoadCleared(placeholderDrawable: Drawable?) {
-            placeholderDrawable?.let {
-                drawable = it
-            }
-        }
-
-        override fun getSize(sizeReadyCallback: SizeReadyCallback) {
-            sizeReadyCallback.onSizeReady(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
-        }
-
-        override fun removeCallback(cb: SizeReadyCallback) {}
-        override fun setRequest(request: Request?) {}
-        override fun getRequest(): Request? {
-            return null
-        }
-
-        override fun onStart() {}
-        override fun onStop() {}
-        override fun onDestroy() {}
-    }
-
-    interface HtmlImagesHandler {
-        fun addImage(uri: String?)
-    }
 }
