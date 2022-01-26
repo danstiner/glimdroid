58b2029fa6bebc0bd6185e95616ca91a8ecb690e
Daniel Stiner <danstiner@gmail.com>
2022-01-26 12:27:13 -0800

    Prefer external storage for cache

--- app/src/main/java/com/danielstiner/glimdroid/GlideModule.kt
+++ app/src/main/java/com/danielstiner/glimdroid/GlideModule.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.Log
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
+import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator
 import com.bumptech.glide.module.AppGlideModule
 
@@ -12,10 +12,11 @@ import com.bumptech.glide.module.AppGlideModule
 @Suppress("unused")
 @GlideModule
 class GlideModule : AppGlideModule() {
+
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         // Reduce disk cache size from default 250MB, we don't have much to cache
         val diskCacheSizeBytes = 10L * 1024 * 1024 * 100 // 10 MB
-        builder.setDiskCache(InternalCacheDiskCacheFactory(context, diskCacheSizeBytes))
+        builder.setDiskCache(ExternalPreferredCacheDiskCacheFactory(context, diskCacheSizeBytes))
 
         // Log memory cache size out of curiosity
         val memsize = MemorySizeCalculator.Builder(context).build()
@@ -24,4 +25,5 @@ class GlideModule : AppGlideModule() {
             "memsize; cacheSize:${memsize.memoryCacheSize}, arrayPoolSize:${memsize.arrayPoolSizeInBytes}, bitmapPoolSize:${memsize.bitmapPoolSize}"
         )
     }
+
 }
